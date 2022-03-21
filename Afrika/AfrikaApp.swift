39363467565b6c48119bc28e8ae5b051b48192ec
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-03-21 16:48:52 +0500

    Initial Commit

---
+++ Afrika/AfrikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfrikaApp.swift
+//  Afrika
+//
+//  Created by Hamza Rafique Azad on 21/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AfrikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
